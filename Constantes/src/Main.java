d3575cd6e09028c5d1c97eb527a22dafe30b3558
inacio <inaciogs03@gmail.com>
2024-07-24 21:58:42 -0300

    conceitos basicos do java

---
+++ Constantes/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		final double pi = 3.14;
+		final int alturaPonte = 4;
+
+	}
+
+}
---
+++ EscapesJava/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		//Escapes
+		System.out.println("Carlos");
+		System.out.println("Silva");
+		System.out.println("-----------------------------");
+		System.out.println("Carlos\n");
+		System.out.println("Silva");
+		System.out.println("-----------------------------");
+		System.out.println("Carlos\t");
+		System.out.println("Silva");
+		System.out.println("-----------------------------");
+		System.out.println("Carlos\\");
+		System.out.println("Silva");
+	}
+
+}
