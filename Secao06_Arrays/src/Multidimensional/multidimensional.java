40803a94214c668f0c995901d627f75cfeafb519
inacio <inaciogs03@gmail.com>
2024-07-28 15:33:55 -0300

    arrays

---
+++ Secao06_Arrays/src/Multidimensional/multidimensional.java
@@ -0,0 +1,16 @@
+package Multidimensional;
+
+public class multidimensional {
+
+	public static void main(String[] args) {
+		String cadastro[][] =
+		{
+			{"Carlos", "4444-4444"}
+			,{"Fernando", "9999-9999"}
+			,{"Fabio", "7777-7777"}
+		};
+		System.out.println(cadastro.length);
+		System.out.println(cadastro[2][0]);
+	}
+
+}
---
+++ Secao06_Arrays/src/Unidimensional/unidimensional.java
@@ -0,0 +1,20 @@
+package Unidimensional;
+
+import java.util.Iterator;
+
+public class unidimensional {
+
+	public static void main(String[] args) {
+		int [] a = new int[4];
+		a[0] = 0;
+		a[1] = 1;
+		a[2] = 2;
+		a[3] = 3;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("a"+i+"="+ a[i]);
+		}
+
+	}
+
+}
